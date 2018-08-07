7e017176e0d18c1977da662125ed337a0afbcaab
Thiago Almeida <thiago_loal@hotmail.com>
2018-08-07 00:33:41 -0300

    corrigindo __dir

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '../public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
