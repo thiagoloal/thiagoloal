feb1c5373219e07b8ffd383cb3cb54a7c9a68269
Thiago Almeida <thiago_loal@hotmail.com>
2018-08-07 00:39:20 -0300

    corrigindo __dir 2

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '../public'));
+app.use(stylus.middleware(__dirname, 'public'));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
